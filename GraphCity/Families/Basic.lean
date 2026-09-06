c7ec0b3cdfba39ebbb5c0cb85ff417e3099a6635
Reuven Peleg <reuven.peleg@mail.huji.ac.il>
2026-09-06 12:17:49 +0300

    Prove number of vertices of complete graph

--- GraphCity/Families/Basic.lean
+++ GraphCity/Families/Basic.lean
@@ -15,4 +15,15 @@ structure GraphFamily where
   /- The graph itself, depening on the param value -/
   graph : (p : Param) → SimpleGraph (Vertex p)
 
+  /- Property: the vertex set is always finite -/
+  vertexFintype : (p : Param) → Fintype (Vertex p)
+
+namespace GraphFamily
+
+/-- The number of vertices of an instance of a finite graph family. -/
+def vertexCard (F : GraphFamily) (p : F.Param) : ℕ :=
+  @Fintype.card (F.Vertex p) (F.vertexFintype p)
+
+end GraphFamily
+
 end GraphCity
--- GraphCity/Families/Complete.lean
+++ GraphCity/Families/Complete.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Reuven Peleg
 -/
 import GraphCity.Basic
+import Mathlib.Data.Fintype.Card
 
 /-!
 Complete graph.
@@ -18,6 +19,12 @@ def family : GraphFamily where
   Param := ℕ
   Vertex n := Fin n
   graph n := SimpleGraph.completeGraph (Fin n)
+  vertexFintype := inferInstance
+
+@[simp]
+theorem card_vertices (n : ℕ) :
+    family.vertexCard n = n := by
+  exact Fintype.card_fin n
 
 end Families.Complete
 end GraphCity
